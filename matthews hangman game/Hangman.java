bcaa12ea63ab01956632093f4ffb103969cf886c
mahttew <50885247+mahttew@users.noreply.github.com>
2019-05-29 11:17:23 -0700

    Update Hangman.java

--- matthews hangman game/Hangman.java
+++ matthews hangman game/Hangman.java
@@ -3,16 +3,16 @@ import java.util.ArrayList;
 
 class Hangman implements GameState
 {
-	private MysteryWord mWord;
-	private Player player;
-	private EasyReader console;
+	private MysteryWord mWord; //mysteryword that returns the word player needs to guess
+	private Player player;     //the player
+	private EasyReader console;// how the console receives guesses and difficulty
 
 	public Hangman()
 	{
 		System.out.print("Choose HARD/MEDIUM/EASY :");
-		console = new EasyReader();
-		String d = console.readLine();
-		mWord = new MysteryWord(d);
+		console = new EasyReader();     //easyreader gets the difficulty from the console. 
+		String d = console.readLine();  //on our final project, the canvas will do this instead.
+		mWord = new MysteryWord(d);     //REMEMBER, d = difficulty, not the actual word. 
 		player = new Player("User");
 		System.out.println("[Enter in a letter to guess!] \n\n");
 		mWord.printGameStatus();
@@ -20,7 +20,8 @@ class Hangman implements GameState
 	
 	public boolean isGameOver()
 	{
-		if(!mWord.noLivesLeft()) return mWord.wordComplete();
+		if(!mWord.noLivesLeft()) return mWord.wordComplete(); // checks if the player has failed three times OR
+								      //if he has guessed the word
 		return true;
 	}
 	
@@ -44,7 +45,7 @@ class Hangman implements GameState
 	
 	public void makeMove(String move)
 	{
-		if(move.equals("guess"))
+		if(move.equals("guess")) //only valid move is guess. so thats what we worry about
 		{
 			mWord.updateGame(console.readChar()+"");
 			mWord.printGameStatus();
@@ -52,7 +53,7 @@ class Hangman implements GameState
 		else return;
 	}
 
-	public String toString()
+	public String toString() //no tostring yet
 	{
 		return "this is toString";
 	}
