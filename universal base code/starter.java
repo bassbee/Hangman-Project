793634fa1cc573a4d69c8f0c0ce101d67906893b
bassbee <42823205+bassbee@users.noreply.github.com>
2019-05-28 20:35:24 -0700

    Update starter.java

--- universal base code/starter.java
+++ universal base code/starter.java
@@ -1,18 +1,41 @@
 import pkg.*;
+import java.util.ArrayList;
+
 public class starter
 {
         public static void main(String args[])
         {
 			ArrayList<Rectangle> parts = new ArrayList<Rectangle>();
 			Picture drNeato = new Picture("neatwhole.png");
+			
 			drNeato.grow(-1100,-1050);
 			drNeato.translate(-1050,-1000);
-			System.out.println(drNeato.getX());
+			
+			System.out.println("x: "+drNeato.getX());
+			System.out.println("y: "+drNeato.getY());
 			System.out.println(drNeato.getWidth() + " " + drNeato.getHeight());
 			drNeato.draw();
-			// Rectangle tester = new Rectangle(0,0,10,10);
-			// tester.setColor(Color.black);
-			// parts.add(tester);
+			
+			Rectangle arm1 = new Rectangle(45,110,140,40);
+			Rectangle arm2 = new Rectangle(260,105,140,50);
+			Rectangle body = new Rectangle(185,105,75,145);
+			Rectangle head = new Rectangle(195,50,45,55);
+			Rectangle leg1 = new Rectangle(185,250,40,165);
+            Rectangle leg2 = new Rectangle(225,250,40,165);
+			
+			arm1.setColor(Color.WHITE);
+			arm2.setColor(Color.WHITE);
+			body.setColor(Color.WHITE);
+			head.setColor(Color.WHITE);
+			leg1.setColor(Color.WHITE);
+			leg2.setColor(Color.WHITE);
+			
+			parts.add(arm1);
+			parts.add(arm2);
+			parts.add(body);
+			parts.add(head);
+			parts.add(leg1);
+			parts.add(leg2);
 		}
 	
 }
