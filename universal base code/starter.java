963b358fb19158051e53e03c58dd58fa1bce9329
Liam Sofias <LSOF8460@stu.gusd.net>
2019-05-30 11:16:40 -0700

    I added the nuse

--- universal base code/starter.java
+++ universal base code/starter.java
@@ -8,6 +8,13 @@ public class starter
 			ArrayList<Rectangle> parts = new ArrayList<Rectangle>();
 			Picture drNeato = new Picture("neatwhole.png");
 			
+			Picture theNuse = new Picture("nuse.png");
+			theNuse.grow(100,100);
+			theNuse.translate(100,100);
+			theNuse.draw();
+			System.out.println("X: "+theNuse.getX());
+			System.out.println("Y: "+theNuse.getY());
+			
 			drNeato.grow(-1100,-1050);
 			drNeato.translate(-1050,-1000);
 			
@@ -16,12 +23,13 @@ public class starter
 			System.out.println(drNeato.getWidth() + " " + drNeato.getHeight());
 			drNeato.draw();
 			
+			int x = 100;
 			Rectangle arm1 = new Rectangle(45,110,140,40);
 			Rectangle arm2 = new Rectangle(260,105,140,50);
 			Rectangle body = new Rectangle(185,105,75,145);
 			Rectangle head = new Rectangle(195,50,45,55);
 			Rectangle leg1 = new Rectangle(185,250,40,165);
-                        Rectangle leg2 = new Rectangle(225,250,40,165);
+            Rectangle leg2 = new Rectangle(225,250,40,165);
 			
 			arm1.setColor(Color.WHITE);
 			arm2.setColor(Color.WHITE);
